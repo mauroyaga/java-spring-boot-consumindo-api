b95b5c64499dc97900e5c96e14558aa8d1730573
Mauro Cesar <mauro.yaga@outlook.com>
2024-09-01 11:48:14 -0300

    estruturando-o-readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 ![Capa do Projeto](assets/diagrama-projeto-conexao-api-java-springboot.png)
+
+
+## Sumário
+
+- [Descrição](#descrição)
+- [Tecnologias](#tecnologias)
+- [Dependências](#dependências)
+- [Instruções](#instruções)
+- [Documentação](#documentação)
+- [Abordagem](#abordagem)
+
+---
+
+## Descrição
+
+
+
+## Tecnologias
+
+
+---
+
+## Dependências
+
+---
+
+## Instruções
+
+---
+
+
+## Documentação
+
+
+---
+
+## Abordagem
+
+
+
--- src/main/java/br/com/alura/sceenmatch/SceenmatchApplication.java
+++ src/main/java/br/com/alura/sceenmatch/SceenmatchApplication.java
@@ -11,10 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.net.URLEncoder;
 
 /*
- * Éssa é uma classe que consome API´s em geral
- * Neste exemplo eu estou consumindo a API do OMDB
- * e retornando o resultado da busca
- * */
+* Éssa é uma classe que consome API´s em geral
+* Neste exemplo eu estou consumindo a API do OMDB
+* e retornando o resultado da busca
+* */
 
 @SpringBootApplication
 
@@ -30,20 +30,8 @@ public class SceenmatchApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         var consumindoAPI = new ConsumindoAPI();
 
-        /*
-         * O conimingAPI.obterDados é um método que recebe uma url que contém a chave de acesso
-         * para consumir a API do OMDB. Extistem alguns parametro que podem ser passados na url
-         * da requisição, como por exemplo o nome do filme que deseja buscar:
-         * Ex: https://omdbapi.com/?t=gilmore+girls&apikey=SUA-CHAVE-DE-ACESSO
-         *
-         * Link para a home da api: https://www.omdbapi.com/
-         * Lá voce pode fazer um cadastro rápido e obter a chave de acesso,
-         * além de ver a documentação da API par entender os tipos de
-         * requisições que podem ser feitas.
-         * */
-
-        var json = consumindoAPI.obterDados("Sua url da requisição com a chave de acesso");
-        System.out.println(json);
+        var json = consumindoAPI.obterDados(  "https://omdbapi.com/?t=gilmore+girls&apikey=848b347e");
+		System.out.println(json);
 
         ConverteDados conversor = new ConverteDados();
 
